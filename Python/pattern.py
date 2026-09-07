90a8ecfd24deaa32ae1a42b58a270133efebcdcb
Preeti Prabhakar Nayak <preeti.pn30@gmail.com>
2026-09-07 07:25:37 +0530

    Refactor pattern printing for upper and lower halves
    Modified the pattern printing logic to include spaces in the lower half.

--- Python/pattern.py
+++ Python/pattern.py
@@ -2,13 +2,11 @@
 
 n = int(input("Enter: "))
 
-# Upper half
 for i in range(1, n + 1):
-    print(" " * (n - i) + "*" * (2 * i - 1))
+    print(" " * (n - i) + "* " * i)
 
-# Lower half
-for i in range(n - 1, 0, -1):
-    print(" " * (n - i) + "*" * (2 * i - 1))
+for i in range(n, 0, -1):
+    print(" " * (n - i) + "* " * i)
 
 
 
